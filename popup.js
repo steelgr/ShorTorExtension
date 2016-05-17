86331a48fc20e37ee50fff4d534273ecbcd039f5
Spyros Panagiotopoulos <martkonsta@gmail.com>
2016-05-18 02:27:05 +0300

    Add options page for custom Server URL

--- popup.js
+++ popup.js
@@ -1,24 +1,29 @@
 document.addEventListener('DOMContentLoaded', function(event) {
-    var test = document.getElementById('titlos');
-    chrome.tabs.query({
-        'active': true,
-        'lastFocusedWindow': true
-    }, function(tab) {
-        $.ajax({
-            url: 'http://127.0.0.1:5000/new',
-            type: 'post',
-            data: {
-                link: tab[0].url
-            },
-            headers: {
-                Accept: 'application/json'
-            },
-            dataType: 'json',
-            success: function(data){
-                console.log(data)
-                test.textContent = data["link"];
+    var test = document.getElementById('result');
+    chrome.storage.sync.get({
+        APIURL: "http://l.daknob.net",
+    }, function(items) {
+        var APIURL = items.APIURL;
+        chrome.tabs.query({
+            'active': true,
+            'lastFocusedWindow': true
+        }, function(tab) {
+            $.ajax({
+                url: APIURL+'/new',
+                type: 'post',
+                data: {
+                    link: tab[0].url
+                },
+                headers: {
+                    Accept: 'application/json'
+                },
+                dataType: 'json',
+                success: function(data) {
+                    console.log(APIURL)
+                    test.textContent = data["link"];
 
-            }
+                }
+            });
         });
     });
 });
